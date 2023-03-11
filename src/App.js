6dd8ed0f2af3aca9b09b700a4b85c9c7da8440d4
NEWCODINGWITHABHI <abhishekkumar01021995@gmail.com>
2023-03-12 02:45:44 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
+const page = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function App() {
+  const [data, setData] = useState([]);
+  const [pageNo, setPageNo] = useState(0);
+  const [pageData,setPageData]=useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        let sliceData = result.slice(pageNo * 10, pageNo * 10 + 10);
+        setPageData(sliceData);
+        setData(result);
+        return result;
+      });
+    
+  }, []);
+  useEffect(() => {
+    let sliceData = data.slice(pageNo * 10, pageNo * 10 + 10);
+    setPageData(sliceData);
+    console.log("pppp")
+  }, [pageNo]);
+
+  function handlePageNext() {
+    if (pageNo < 10) {
+      setPageNo(pageNo + 1);
+    }
+  }
+  function handlePagePrev() {
+    if (pageNo > 0) {
+      setPageNo(pageNo - 1);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        {pageData.map((item) => {
+          return (
+            <div className="img-box" key={item.id}>
+              <img src={item.url} alt="photo" />
+              <p>{item.title}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="pagination-box">
+        {pageNo == 0 ? null : (
+          <button onClick={() => handlePagePrev()}>Prev</button>
+        )}
+
+        {page.map((p, index) => {
+          return (
+            <span
+              style={{ backgroundColor: `${pageNo == p ? "grey" : ""}` }}
+              key={index}
+            >
+              {p}
+            </span>
+          );
+        })}
+        {pageNo == 10 ? null : (
+          <button onClick={() => handlePageNext()}>Next</button>
+        )}
+      </div>
     </div>
   );
 }
